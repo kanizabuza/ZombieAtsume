f97a28148fe0abec1718c6c20ce8b0fe02915edd
honononn <50857157+honononn@users.noreply.github.com>
2019-07-04 03:37:14 +0900

    tyou kasukasu

---
+++ ZombieAtsume/Assets/Shiori/Scripts/Human.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+    public Player playersc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("2");
+        if (other.tag == "attack")
+        {
+            Debug.Log("1");
+            playersc.destroyflag = 1;
+
+            playersc.Burn(other);
+        }
+    }
+}
--- ZombieAtsume/Assets/Shiori/Scripts/Player.cs
+++ ZombieAtsume/Assets/Shiori/Scripts/Player.cs
@@ -9,6 +9,11 @@ public class Player : MonoBehaviour
     public float speed = 3f;
     float moveX = 0f;
     float moveZ = 0f;
+    float time = 0;
+
+    int attackflag = 0;
+    public int destroyflag = 0;
+    int burnflag = 0;
     Rigidbody rb;
     private Animator anim;
     // Start is called before the first frame update
@@ -27,15 +32,28 @@ public class Player : MonoBehaviour
         {
             var lookPoint = ray.GetPoint(distance);
             transform.LookAt(lookPoint);
-        }
 
+        }
         moveX = Input.GetAxis("Horizontal") * speed;
         moveZ = Input.GetAxis("Vertical") * speed;
         Vector3 direction = new Vector3(moveX, 0, moveZ);
-        Debug.Log("1");
+        time += Time.deltaTime;
+        if(attackflag == 1)
+        {
+            if (time < 10f && destroyflag == 1)
+            {
+                burnflag = 1;
+
+            }
+            else
+            {
+                attackflag = 0;
+                burnflag = 0;
+            }
+        }
         if (moveX != 0 || moveZ != 0)
         {
-            Debug.Log("2");
+        
             anim.SetBool("Walk", true);
         }
 
@@ -45,10 +63,26 @@ public class Player : MonoBehaviour
         }
         if (Input.GetMouseButtonDown(0))
         {
+            attackflag = 1;
             anim.SetTrigger("Attack");
         }
     }
 
+
+    public void Burn(Collider obj)
+    {
+        Debug.Log(burnflag);
+        Debug.Log("10");
+        Debug.Log(attackflag);
+
+        if (burnflag == 1 && attackflag == 1)
+        {
+            Debug.Log("3");
+            Destroy(obj);
+        }
+        destroyflag = 0;
+
+    }
     private void FixedUpdate()
     {
         rb.velocity = new Vector3(moveX, 0, moveZ);
