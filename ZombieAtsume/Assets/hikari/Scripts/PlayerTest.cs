ebbeab2ee3044720ad6d87285f9b0b711fe983ea
hika04157726@gmail.com <hika.04157726@gmail.com>
2019-07-04 03:31:31 +0900

    edit player change process

---
+++ ZombieAtsume/Assets/hikari/Scripts/PlayerTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTest : MonoBehaviour
+{
+    private bool isControl;
+
+    void Update() {
+
+    }
+
+    public void ChangeControl(bool flag) {
+        isControl = flag;
+        this.GetComponent<MeshRenderer>().enabled = flag;
+    }
+}
