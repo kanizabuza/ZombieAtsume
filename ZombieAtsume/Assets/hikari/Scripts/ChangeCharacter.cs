8c3f8ea0a155ec9136cdd800727d70484519dd4e
hika04157726@gmail.com <hika.04157726@gmail.com>
2019-07-04 02:33:16 +0900

    edit scripts

--- ZombieAtsume/Assets/hikari/Scripts/ChangeCharacter.cs
+++ ZombieAtsume/Assets/hikari/Scripts/ChangeCharacter.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class ChangeCharacter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private int nowPlayer;
+
+    [SerializeField] private List<GameObject> charaLists;
+
+    private void Start() {
+        nowPlayer = charaLists.Count;
+        ChangePlayer(nowPlayer);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        if (Input.GetKeyDown("q")){
+            ChangePlayer(nowPlayer);
+        }
+    }
+
+    void ChangePlayer(int tempNowPlayer) {
+        bool flag;
+        int nextPlayer = tempNowPlayer + 1;
+
+        //循環処理
+        if (nextPlayer >= charaLists.Count)
+        {
+            nextPlayer = 0;
+        }
+
+        for(int i=0; i<charaLists.Count; i++)
+        {
+            if(i == nextPlayer)
+            {
+                flag = true;
+            }
+            else
+            {
+                flag = false;
+            }
+
+            //変換処理
+            charaLists[i].GetComponent<MeshRenderer>().enabled = flag;
+        }
+        nowPlayer = nextPlayer;
     }
 }
