2992dad33ee76a18dc755c7ac2f8b27ef8a911c4
hika04157726@gmail.com <hika.04157726@gmail.com>
2019-07-06 17:11:24 +0900

    edit playerTest.cs

--- ZombieAtsume/Assets/hikari/Scripts/PlayerTest.cs
+++ ZombieAtsume/Assets/hikari/Scripts/PlayerTest.cs
@@ -6,19 +6,41 @@ using UnityEngine.AI;
 
 public class PlayerTest : MonoBehaviour
 {
-
-    private int zombieHP = 100;
+    private Image fillImg;
+    private Color fullHpColor = Color.green;
+    private Color zeroHpColor = Color.red;
+    private int zombieHp;
+    private int maxHp = 100;
 
     private GameObject hpUI;
     private Slider hpSlider;
 
     private void Start() {
         hpSlider = GetComponentInChildren<Slider>();
+        fillImg = hpSlider.image;
     }
 
     void LateUpdate() {
         transform.rotation = Camera.main.transform.rotation;
     }
 
+    public void TakeDamage(int damage) {
+        zombieHp -= damage;
 
+        SetHpUI();
+
+        if(zombieHp <= 0f)
+        {
+            OnDeath();
+        }
+    }
+
+    private void SetHpUI() {
+        hpSlider.value = zombieHp;
+        fillImg.color = Color.Lerp(zeroHpColor, fullHpColor, zombieHp / maxHp);
+    }
+
+    private void OnDeath() {
+        Destroy(this.gameObject);
+    }
 }
