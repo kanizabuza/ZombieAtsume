16da0937525669f5309e6fbe9fc19debc257d405
hika04157726@gmail.com <hika.04157726@gmail.com>
2019-07-06 14:29:04 +0900

    add scripts

---
+++ ZombieAtsume/Assets/hikari/Scripts/FollowZombie.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowZombie : MonoBehaviour
+{
+    GameObject target;
+    NavMeshAgent agent;
+
+    [SerializeField]
+    private float arrivedDistance = 3f;
+
+    [SerializeField]
+    private float followDistance = 4f;
+
+    private void Start() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update() {
+        target = GameObject.FindGameObjectWithTag("Player");
+        agent.SetDestination(target.transform.position);
+        if(agent.remainingDistance < arrivedDistance)
+        {
+            agent.isStopped = true;
+        }else if (agent.remainingDistance > followDistance)
+        {
+            agent.isStopped = false;
+        }
+
+    }
+
+}
